be056f6e9f9482918d6189017a84478d7b922578
sikander-afzaal <72333576+sikander-afzaal@users.noreply.github.com>
2022-04-15 15:27:30 +0500

    first commit

--- src/pages/Home/Siege.jsx
+++ src/pages/Home/Siege.jsx
@@ -1,13 +1,16 @@
 import React from "react";
-
+import hammerLeft from "../../assets/icons/hammer-left.svg";
+import hammerRight from "../../assets/icons/hammer-right.svg";
 function Siege() {
   return (
-    <div className="siege box">
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <div className="content">
+    <div className="siege">
+      <img src={hammerLeft} alt="" className="hammer-left" />
+      <img src={hammerRight} alt="" className="hammer-right" />
+      <div className="content box">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
         <p>
           Mysterious forces have begun laying siege upon the realms of
           Yggdrasil, the tree of worlds. Odin has called upon the Gods of Asgard
